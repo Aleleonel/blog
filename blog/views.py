6fed016957897d819cd88c82904b3425c0201a77
Aleleonel <aleleonel@gmail.com>
2022-12-20 15:57:15 -0300

    configurações iniciais'

---
+++ blog/views.py
@@ -0,0 +1,33 @@
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from django.shortcuts import get_object_or_404, render
+from django.views.generic import ListView
+
+from .models import Post
+
+
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
+
+
+def post_list(request):
+    object_list = Post.published.all()
+    paginator = Paginator(object_list, 3)
+    page = request.GET.get('page')
+
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        # se a pagina não for um inteiro, exibe a primeira pagina
+        posts = paginator.page(1)
+    except EmptyPage:
+        # Se a pagina estiver fora do intervalo,
+        # exibe a última pagina de resultado.
+        posts = paginator.page(paginator.num_pages)
+    return render(request, 'blog/post/list.html', {'page':page, 'posts': posts})
+
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(Post, slug=post, status='published', publish__year=year, publish__month=month, publish__day=day)
+    return render(request, 'blog/post/detail.html', {'post':post})
